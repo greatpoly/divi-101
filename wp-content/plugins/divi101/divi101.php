841c085f68753604fa2a1bf9df243730e8b7359c
shimul <shimuluse@gmail.com>
2018-04-10 08:58:57 +0600

    Plugin meta info : (Plugin Name, Plugin URI, Description etc)

--- wp-content/plugins/divi101/divi101.php
+++ wp-content/plugins/divi101/divi101.php
@@ -1,7 +1,24 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: shimu
- * Date: 4/9/2018
- * Time: 9:59 PM
+ * Plugin Name: Divi 101
+ * Plugin URI:  https://divi101.com
+ * Description: Divi 101 is a tutorial plugin, which allows to view tutorial within admin panel, saving a ton of time.
+ * Version:     1.0
+ * Author:      ShiftWeb Solutions
+ * Author URI:  https://shiftwebsolutions.com/
+ * License:     GPLv2 or later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * with this program. If not, visit: https://www.gnu.org/licenses/
+ *
  */
