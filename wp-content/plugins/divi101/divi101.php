d519e46ad6cdf9e89ec4ae46f19a337e70d84023
shimul <shimuluse@gmail.com>
2018-04-11 21:38:37 +0600

    Adding first level and sub level menus

--- wp-content/plugins/divi101/divi101.php
+++ wp-content/plugins/divi101/divi101.php
@@ -35,25 +35,117 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Display the plugin settings page
-function divi101_display_settings_page () {
+function divi101_display_settings_page() {
 	// check if user is allowed access
-	if ( !current_user_can('manage_options')) return ;
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
 	?>
 
-	<div class="wrap">
-		<h1><?php echo esc_html( get_admin_page_title()); ?></h1>
-		<form action = "options.php" method="post">
+    <div class="wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+        <form action="options.php" method="post">
 			<?php
 			// output security fields
-			settings_fields ('divi101_options');
+			settings_fields( 'divi101_options' );
 
 			// output setting sections
-			do_settings_sections('myplugin');
+			do_settings_sections( 'divi101' );
 
 			// submit button
 			submit_button();
 			?>
-		</form>
-	</div>
-<?php
+        </form>
+    </div>
+	<?php
 }
+
+// add top-level administrative menu
+function divi101_add_top_level_menu() {
+	/*
+		add_menu_page {
+				string      $page_title,
+				string      $menu_title,
+				string      $capability,
+				string      $menu_slug,
+				callable    $function = '',
+				string      $icon_url = '',
+				int         $position = null
+	*/
+	add_menu_page(
+		'Welcome to Divi 101',
+		'Divi 101',
+		'manage_options',
+		'divi101',
+		'divi101_display_settings_page',
+		'dashicons-tickets',
+		null
+	);
+}
+
+add_action( 'admin_menu', 'divi101_add_top_level_menu' );
+
+add_action( 'admin_head', 'my_custom_favicon' );
+function my_custom_favicon() {
+	echo '
+    <style>
+    .dashicons-tickets {
+        background: url("' . plugins_url( '/images/divi_101_icon.png', __FILE__ ) . '") no-repeat center;
+        
+    }
+    .menu-top:hover .dashicons-tickets {
+        background: url("' . plugins_url( '/images/divi_101_icon_b.png', __FILE__ ) . '") no-repeat center;
+        
+    }
+    .dashicons-tickets:before {
+        content: none;
+    }
+    .current .dashicons-tickets {
+        background: url("' . plugins_url( '/images/divi_101_icon_w.png', __FILE__ ) . '") no-repeat center;
+        
+    }
+    .current:hover .dashicons-tickets {
+        background: url("' . plugins_url( '/images/divi_101_icon_w.png', __FILE__ ) . '") no-repeat center;
+        
+    }
+    </style>
+';
+}
+
+function divi101_display_inner_pages() {
+	// check if user is allowed access
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+	?>
+
+    <div class="wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+        <form action="options.php" method="post">
+			<?php
+			// output security fields
+			settings_fields( 'divi101_options' );
+
+			// output setting sections
+			do_settings_sections( 'divi101' );
+
+			// submit button
+			submit_button();
+			?>
+        </form>
+    </div>
+	<?php
+}
+
+function divi101_add_sublevel_menu (){
+	add_submenu_page (
+		'divi101',
+		'Divi Registration',
+		'Divi Registration',
+		'manage_options',
+		'divi_registration',
+		'divi101_display_inner_pages'
+	);
+}
+
+add_action('admin_menu', 'divi101_add_sublevel_menu');
