f0ad9ae6e86df830c3a6ba6ede179ce87f37c7eb
shimul <shimuluse@gmail.com>
2018-04-11 17:09:29 +0600

    Function for displaying plugin settings in the wp admin left top menu

--- wp-content/plugins/divi101/divi101.php
+++ wp-content/plugins/divi101/divi101.php
@@ -34,3 +34,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+// Display the plugin settings page
+function divi101_display_settings_page () {
+	// check if user is allowed access
+	if ( !current_user_can('manage_options')) return ;
+	?>
+
+	<div class="wrap">
+		<h1><?php echo esc_html( get_admin_page_title()); ?></h1>
+		<form action = "options.php" method="post">
+			<?php
+			// output security fields
+			settings_fields ('divi101_options');
+
+			// output setting sections
+			do_settings_sections('myplugin');
+
+			// submit button
+			submit_button();
+			?>
+		</form>
+	</div>
+<?php
+}
